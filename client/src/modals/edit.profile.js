93d3e351bc293cee4e6ed51a394aef534ee396c5
Giang <76941207+Giang-github@users.noreply.github.com>
2024-03-16 11:26:24 +0700

    delete client

--- client/src/modals/edit.profile.js
+++
@@ -1,3 +0,0 @@
-const EditProfileForm = ({ user }) => {};
-
-export default EditProfileForm;
--- client/src/services/api.service.js
+++
@@ -1,6 +0,0 @@
-const publicApi = {
-  auth: "http://localhost:5000/api/auth/",
-  faculty: "http://localhost:5000/api/faculty/",
-  user: "http://localhost:5000/api/user-management/",
-};
-export default publicApi;
